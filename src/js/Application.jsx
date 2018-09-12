dd9c72cc6387b195c54febf1cbd835a381ae0bbb
Astrid Varga <astrid.varga@gmx.de>
2018-09-12 16:34:51 +0200

    Add react setup

---
+++ src/js/Application.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Application extends React.Component {
+    render() {
+        return (
+            <div className="application">
+                <h1>Our first React Code</h1>
+            </div>
+        );
+    }
+}
+
+export default Application;
